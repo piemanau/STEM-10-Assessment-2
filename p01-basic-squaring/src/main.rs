374da02dfb6ac8cd77ee46d864b3fcd444df83ed
LimeShulkerBox <77601849+piemanau@users.noreply.github.com>
2023-03-27 09:43:43 +1100

    P01 Basic Squaring Complete

---
+++ p01-basic-squaring/src/main.rs
@@ -0,0 +1,19 @@
+use yew::prelude::*;
+use yew::{html};
+
+mod components;
+
+use crate::components::number_input::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <NumberInput name="input"/>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
