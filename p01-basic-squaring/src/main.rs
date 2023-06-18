5077fb283983ed7428847320a7ccf4976b8f5fea
LimeShulkerBox <77601849+piemanau@users.noreply.github.com>
2023-06-19 09:27:12 +1000

    Comment squaring app

--- p01-basic-squaring/src/main.rs
+++ p01-basic-squaring/src/main.rs
@@ -6,25 +6,39 @@ use yew::{function_component, Html, Callback, html};
 #[function_component]
 fn App() -> Html {
     
+    // When input is detected it squares the number and puts it on the output element
     let oninput = Callback::from(|event: InputEvent| {
+
+        // Grabs the input value
         let value = event.clone()
             .target()
             .unwrap()
             .unchecked_into::<HtmlInputElement>()
             .value();
+
+        // Parses the input to i128
         let num_value = value.parse::<i128>().unwrap_or(0);
+
+        // Updates the output and gets the square
         document().get_element_by_id("output").unwrap().set_inner_html((num_value * num_value).to_string().as_str());
     });
 
+
+    // Html for title, input and output
     html! {
         <div>
+            // Header (title)
             <h1>{"Squaring Calculator"}</h1>
+            // Input element, calls input when it gets some input
             <input type="number" name={"input"} oninput={oninput} placeholder="Number"/>
-            <div id="output"></div>
+            // Output element
+            <div id="output">{"0"}</div>
         </div>
     }
 }
 
+
+// Main function, where the code starts and calls the app.
 fn main() {
     yew::Renderer::<App>::new().render();
 }
