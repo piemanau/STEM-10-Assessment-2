464fdbbd304bf65fafdffbbb549bc54e406ad5ae
LimeShulkerBox <77601849+piemanau@users.noreply.github.com>
2023-06-24 22:54:18 +1000

    make p01 more idiomatic

--- p01-basic-squaring/src/main.rs
+++ p01-basic-squaring/src/main.rs
@@ -1,27 +1,37 @@
-use gloo::{utils::document};
 use wasm_bindgen::JsCast;
 use web_sys::{InputEvent, HtmlInputElement};
-use yew::{function_component, Html, Callback, html};
+use yew::{function_component, Html, Callback, html, use_state};
 
 #[function_component]
 fn App() -> Html {
     
-    // When input is detected it squares the number and puts it on the output element
-    let oninput = Callback::from(|event: InputEvent| {
+    // Value that goes on the output element
+    let value = use_state(|| 0);
 
+    // When input is detected it squares the number and puts it on the output element
+    let oninput: Callback<InputEvent> = {
+
+        // Create a copy of the value variable
+        let value = value.clone();
+
+       Callback::from(move |event: InputEvent| {
         // Grabs the input value
-        let value = event.clone()
+        let num_value = event
             .target()
             .unwrap()
             .unchecked_into::<HtmlInputElement>()
             .value();
 
-        // Parses the input to i128
-        let num_value = value.parse::<i128>().unwrap_or(0);
+            // Parses the input to i128
+            let num_value = num_value.parse::<i128>().unwrap_or(0);
 
-        // Updates the output and gets the square
-        document().get_element_by_id("output").unwrap().set_inner_html((num_value * num_value).to_string().as_str());
-    });
+            // Squares the number
+            let num_value = num_value * num_value;
+
+            // Updates the output
+            value.set(num_value);
+        })
+    };
 
 
     // Html for title, input and output
@@ -30,9 +40,9 @@ fn App() -> Html {
             // Header (title)
             <h1>{"Squaring Calculator"}</h1>
             // Input element, calls input when it gets some input
-            <input type="number" name={"input"} oninput={oninput} placeholder="Number"/>
-            // Output element
-            <div id="output">{"0"}</div>
+            <input type="number" name={"input"} {oninput} placeholder="Number"/>
+            // Output element, dereference the value
+            <div id="output">{ *value }</div>
         </div>
     }
 }
