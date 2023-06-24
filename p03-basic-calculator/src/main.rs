20c3cc0544b0950c690d5c1039c231f57564fc81
LimeShulkerBox <77601849+piemanau@users.noreply.github.com>
2023-06-24 23:54:48 +1000

    remove useless enums and add comments to p03

--- p03-basic-calculator/src/main.rs
+++ p03-basic-calculator/src/main.rs
@@ -6,19 +6,7 @@ use crate::components::button::Button;
 
 mod components;
 
-#[derive(PartialEq, Clone)]
-pub enum State {
-    NumberOne(CustomNumber),
-    Operation(Operation),
-    NumberTwo(CustomNumber),
-}
-
-#[derive(PartialEq, Clone)]
-pub enum CustomNumber {
-    Number(f64),
-    None,
-}
-
+// All the different operations, when passing in it HAS to be one of these due to no null in rust
 #[derive(PartialEq, Clone, Copy)]
 pub enum Operation {
     Multiplication,
@@ -33,12 +21,14 @@ pub enum Operation {
     AllClear,
 }
 
+// Tells the button what it does
 #[derive(PartialEq, Clone, Copy)]
 pub enum Value {
     Number(i8),
     Operation(Operation),
 }
 
+// Lets me make use of a hacky workaround by pattern matching and getting the value from the enum
 #[derive(PartialEq, Clone, Copy)]
 pub enum OuterValue {
     Value(Value),
@@ -55,7 +45,7 @@ fn App() -> Html {
                     <p class="output text" style="border-bottom-right-radius: 2.5% 10%; border-bottom-left-radius: 2.5% 10%;"><p class="inner-output text" id="output">{""}</p></p>
                 </div>
 
-                // Adds all the button, including numbers and operations
+                // Adds all the button, including numbers and operations. Uses the button component, see src/components/button.rs
                 <div class="button zero"><Button value={OuterValue::Value(Value::Number(0))} text="0"/></div>
                 <div class="button one"><Button value={OuterValue::Value(Value::Number(1))} text="1"/></div>
                 <div class="button two"><Button value={OuterValue::Value(Value::Number(2))} text="2"/></div>
